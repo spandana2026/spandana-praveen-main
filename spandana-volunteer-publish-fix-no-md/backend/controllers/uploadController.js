94b8dce2e1e61f53e825ac9b68329c23d42a35d8
Praveen Seelam <spandanacareaidfoundation@gmail.com>
2026-09-11 19:04:33 +0530

    removed unwanted files

--- spandana-volunteer-publish-fix-no-md/backend/controllers/uploadController.js
+++
@@ -1,6 +0,0 @@
-import { fileToUrl } from '../services/uploadService.js';
-
-export async function upload(req, res) {
-  if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
-  res.json({ url: fileToUrl(req.file.filename) });
-}
